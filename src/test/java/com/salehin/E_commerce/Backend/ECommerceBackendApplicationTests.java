459957024a34299c3ffa01251db18ff27e81cd51
SHERAJUS SALEHIN <66803588+Sherajus-Salehin@users.noreply.github.com>
2026-04-18 23:43:15 +0600

    init

---
+++ src/test/java/com/salehin/E_commerce/Backend/ECommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salehin.E_commerce.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
