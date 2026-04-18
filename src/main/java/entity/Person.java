9109f2857f0a3c2f09f1c709f1be0f4f18b594d4
SHERAJUS SALEHIN <66803588+Sherajus-Salehin@users.noreply.github.com>
2026-04-19 01:50:13 +0600

    added Person class and Readme file

---
+++ src/main/java/entity/Person.java
@@ -0,0 +1,19 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Table(name = "people")
+@Data
+@RequiredArgsConstructor
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+    private String firstName;
+    private String lastName;
+    private String lock; //email
+    private String key;
+}
